d4c4cfb3403764db4b6103f316c7df03186478b9
Jacek Byzdra <jacekbwwa@gmail.com>
2025-06-13 16:54:59 +0200

    Delete TravelAgencyWebDesignProjectJB directory

--- TravelAgencyWebDesignProjectJB/js/backpacker.js
+++
@@ -1,9 +0,0 @@
-document.body.onload = function() {
-			let start = Date.now();
-			let moveBackPacker = setInterval(function() {
-				let timeElapsed = Date.now() - start;
-				backpacker.style.bottom = timeElapsed / 2 + 'px';
-				if (timeElapsed > 4000) clearInterval(moveBackPacker);
-
-			}, 20);
-		}
